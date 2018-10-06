3299411f7ad9299a8ef81b9b51663bd0e0cf738b
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-10-06 14:34:09 -0400

    shows up in table

--- javascript.js
+++ javascript.js
@@ -20,8 +20,8 @@ var config = {
       //grabs user input
       var trainName = $("#train-input").val().trim();
       var trainDestination = $("#destination-input").val().trim();
-      var firstTrainTime = $("#first-train-input").val().trim();
-      var trainFrequency = $("#frequency-input").val().trim();
+      var firstTrainTime = $("#frequency-input").val().trim();
+      var trainFrequency = $("#first-train-input").val().trim();
 
       //local "temporary" object for holding train data
       var newTrain = {
@@ -34,9 +34,14 @@ var config = {
       //uploads train data to database
       database.ref().push(newTrain);
 
+      console.log(newTrain.name);
+      console.log(newTrain.destination),
+      console.log(newTrain.time),
+      console.log(newTrain.frequency),
+
       //clears all text boxes
       $("#train-input").val("");
-      $("#destintion-input").val("");
+      $("#destination-input").val("");
       $("#first-train-input").val("");
       $("#frequency-input").val("");
 });
@@ -47,17 +52,45 @@ database.ref().on("child_added", function(childShapshot) {
     //store into variable
     var trainName = childShapshot.val().name;
     var trainDestination = childShapshot.val().destination;
-    var firstTrainTime = childShapshot.val().time;
     var trainFrequency = childShapshot.val().frequency;
+    var firstTrainTime = childShapshot.val().time;
+   
 
     //create new row
-    var newRow = ("<tr>").append(
+    var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDestination),
-        $("<td>").text(firstTrainTime),
         $("<td>").text(trainFrequency),
+        $("<td>").text(firstTrainTime),
+        $("<td>").text(tMinutesTillTrain),
     );
 
     //append new row to table
     $("#train-table > tbody").append(newRow);
 });
+
+//train time math
+var tFrequency = $("#frequency-input");
+
+var firstTime = $("#first-train-input");
+
+var timeConverted = moment(firstTime, "HH:mm").subtract(1, "year");
+console.log(timeConverted);
+
+var currentTime = moment();
+console.log("current time: " + moment(currentTime).format("hh:mm"));
+
+var diffTime = moment().diff(moment(timeConverted), "minutes");
+diffTime = parseInt(diffTime);
+console.log("difference in time: " + diffTime);
+
+var tRemainder = diffTime % tFrequency;
+tRemainder = parseInt(tRemainder);
+console.log(tRemainder);
+
+var tMinutesTillTrain = tFrequency - tRemainder;
+tMinutesTillTrain = parseInt(tMinutesTillTrain);
+console.log("minutes till train: " + tMinutesTillTrain);
+
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("arrival time: " + moment(nextTrain).format("hh:mm"));
